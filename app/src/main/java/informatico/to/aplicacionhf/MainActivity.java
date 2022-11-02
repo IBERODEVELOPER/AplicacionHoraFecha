4693e5bd5956298f2b97c51220543bc5b8e4bc6b
Raul Tamani <tamani1995@gmail.com>
2022-11-02 13:57:52 -0500

    update 2022

--- app/src/main/java/informatico/to/aplicacionhf/MainActivity.java
+++ app/src/main/java/informatico/to/aplicacionhf/MainActivity.java
@@ -8,12 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
     //creamos objetos de las clases
-    Button btnDate;
-    TextView vt;
+    Button btndate;
+    TextView tvfecha;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +26,12 @@ public class MainActivity extends AppCompatActivity {
         //llamamo a la imagen por su nombre que tiene que estar en drawable
         getSupportActionBar().setIcon(R.drawable.ic_action_name);
         //instanciar la variable local con id  de los componentes del xml
-        btnDate=(Button)findViewById(R.id.btnDate);
-        vt=(TextView)findViewById(R.id.txtview);
+        btndate=(Button)findViewById(R.id.btndate);
+        tvfecha=(TextView)findViewById(R.id.tvfecha);
+
         /*creamos un objeto de una clase y llamamo al metodo de la clase*/
         Time today=new Time(Time.getCurrentTimezone());
+
         /*Optenemos la fecha actual */
         today.setToNow();
         /*Creamos variables int y llamamos a los atributos de la clase time*/
@@ -35,13 +40,22 @@ public class MainActivity extends AppCompatActivity {
         int ano=today.year;
         /*El mes lo sumamos mas 1 por que la clase time me da un mes atrasado */
         mes=mes+1;
-        vt.setText("Fecha   Dia : " + dia + " Mes : "+mes+" Año : "+ano);
+       // tvfecha.setText("Fecha   Dia : " + dia + " Mes : "+mes+" Año : "+ano);
 
-        btnDate.setOnClickListener(new View.OnClickListener() {
+        btndate.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        btnDate.setText(new Date().toString());
+                        btndate.setText(new Date().toString());
                     }
                 });
+
+        Calendar calendar = Calendar.getInstance();
+        int dianow=calendar.get(Calendar.DATE);
+        int mesnow=calendar.get(Calendar.MONTH);
+        int anionow=calendar.get(Calendar.YEAR);
+
+        /*El mes lo sumamos mas 1 por que la clase time me da un mes atrasado */
+        mesnow++;
+        tvfecha.setText("Fecha   Dia : " + dianow + " Mes : "+mesnow+" Año : "+anionow);
     }
 }
